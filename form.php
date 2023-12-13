5655811d59831e7b2b0ccf86f60587b1df3b8e29
Miguel <142063312+kakakaue@users.noreply.github.com>
2023-12-13 19:51:19 -0300

    ..........
    php terminado

--- form.php
+++ form.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="form.css">
+    <link rel="stylesheet" href="formulario.css">
+
 </head>
 <body>
 
@@ -20,6 +21,8 @@
 
             echo "<p> Obrigado  por cadastrar-se em nosso site $nome Esperamos que desfrute de uma ótima experiência, agradecemos pela preferência e confiança em nossos serviços!<\p>"
         ?>
+
+        <br><br>
 
         <button class = "button-home" >
             <a href="index.html"> Voltar a página inicial</a>
