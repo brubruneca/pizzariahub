ee9a455b8ec33991347dcc288be6225f495a2d4f
Wellington da Silva <142063312+kakakaue@users.noreply.github.com>
2023-12-12 21:37:56 -0300

    ...............
    opa
    
    Co-Authored-By: zlgsilva <142062585+zlgsilva@users.noreply.github.com>
    Co-Authored-By: RenatoMoraes.DBA <142064719+RenatoMoraesDBA@users.noreply.github.com>
    Co-Authored-By: Ed Carlos Confessor  Dev. <148491306+EdCarlosConfessor@users.noreply.github.com>

--- form.php
+++ form.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="form-html">
 </head>
 <body>
-<<<<<<< Updated upstream
 
 <header>
         <h1>Cadastro de Clientes | Pizza HUB</h1>
@@ -15,26 +14,17 @@
 
     <main class = "text">
         <?php
-            $nome = $_GET ["nome"];
-            $telefone = $_GET ["telefone"];
-            $email = $_GET ["email"];
+            $nome = $_GET [ "nome"]
+            $telefone = $_GET [ "telefone"]
+            $email = $_GET ["email"]
 
-            echo "<p> Obrigado  por cadastrar-se em nosso site $nome Esperamos que desfrute de uma ótima experiência, agradecemos pela preferência e confiança em nossos serviços! <\p>"
-=======
-   <header>
-     <h1>Resultado do processamento</h1>
-   </header>
-   
-   <main>
-        <?php
-           $nome = $_GET["nome"];
-           $sobrenome = $_GET["sobrenome"];
-
-           echo "É um prazer te conhecer, $nome $sobrenome! Este é meu site";
->>>>>>> Stashed changes
+            echo "<p> Obrigado  por cadastrar-se em nosso site $nome Esperamos que desfrute de uma ótima experiência, agradecemos pela preferência e confiança em nossos serviços!<\p>"
         ?>
-        <p><a href="javascript:history.go(-1)">Voltar para página anterior</a></p>
-   </main>
-  
+
+        <button class = "button-home" >
+            <a href="index.html"> Voltar a página inicial</a>
+        </button>
+    </main>
+
 </body>
 </html>
