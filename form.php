c66dcf0e116b166f3b44f7e5215201124845d7ef
Renato <renatomoraes.dba@gmail.com>
2023-12-12 21:20:25 -0300

    tcc

--- form.php
+++ form.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="form-html">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<<<<<<< Updated upstream
 
 <header>
         <h1>Cadastro de Clientes | Pizza HUB</h1>
@@ -19,12 +20,21 @@
             $email = $_GET ["email"];
 
             echo "<p> Obrigado  por cadastrar-se em nosso site $nome Esperamos que desfrute de uma ótima experiência, agradecemos pela preferência e confiança em nossos serviços! <\p>"
+=======
+   <header>
+     <h1>Resultado do processamento</h1>
+   </header>
+   
+   <main>
+        <?php
+           $nome = $_GET["nome"];
+           $sobrenome = $_GET["sobrenome"];
+
+           echo "É um prazer te conhecer, $nome $sobrenome! Este é meu site";
+>>>>>>> Stashed changes
         ?>
-
-        <button class = "button-home" >
-            <a href="index.html"> Voltar a página inicial</a>
-        </button>
-    </main>
-
+        <p><a href="javascript:history.go(-1)">Voltar para página anterior</a></p>
+   </main>
+  
 </body>
 </html>
